22fe86415499f220167dcda8f3aa2341ddff6d0c
Bas Maas <bas.maas@sogeti.com>
2020-08-03 19:42:52 +0200

    flex layout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { TDEEService } from './tdee.service';
+import { MediaObserver, MediaChange } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,25 @@ import { TDEEService } from './tdee.service';
   styleUrls: ['./app.component.css'],
   providers: [TDEEService]
 })
-export class AppComponent {
-  title = 'angu';
+export class AppComponent implements OnInit, AfterViewInit{
+
+  constructor(
+    private mediaObserver: MediaObserver,
+  ){
+
+  }
+
+  ngOnInit(){
+    this.mediaSub = this.mediaObserver.media$.subscribe(
+      (change: MediaChange) => {console.log(change.mqAlias)}
+    )
+  }
+
+  ngAfterViewInit(){
+
+  }
+
+  private mediaSub: Subscription;
+
+
 }
