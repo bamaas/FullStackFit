dae53c9c0cdc29de0249189ef30d23c95cdce9f4
Bas Maas <bas.maas@sogeti.com>
2020-09-04 22:35:41 +0200

    added mobile device e2e tests on samsung galaxy s10 plus

--- test/helpers/SetupHelper.py
+++ test/helpers/SetupHelper.py
@@ -1,19 +1,24 @@
-import os
 from robot.api import logger
+from browserstack.local import Local
+from dotenv import load_dotenv
 
-def load_env_file(filepath):
-    from dotenv import load_dotenv
-    load_dotenv(dotenv_path=filepath)
+class SetupHelper():
 
-def connect_bs_to_local(key):
-    """ Create a connection between Browserstack and localhost """
-    from browserstack.local import Local
-    bs_local = Local()
-    bs_local_args = { "key": key, "forcelocal": "true", }
-    bs_local.start(**bs_local_args)
-    logger.info('Browerstack local connection:', bs_local.isRunning())
-    # The default Browserstack local.stop() isn't working for some reason. 
-    # But actually there is no need to close the connection as it is running inside a Docker container that gets killed after the test is done.
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
 
-# def stop_bs_local():
-#     bs_local.stop()
+    def __init__(self):
+        self.bs_local = Local()
+        
+    def load_env_file(self, filepath):
+        load_dotenv(dotenv_path=filepath)
+
+    def connect_bs_to_local(self, key):
+        """ Create a connection between Browserstack and localhost """
+        bs_local_args = { "key": key, "forcelocal": "true", }
+        if not self.bs_local.isRunning():
+            self.bs_local.start(**bs_local_args)
+            logger.info('Created Browserstack local connection.')
+
+    def disconnect_from_browserstack(self):
+        self.bs_local.stop()
+        logger.info('Stopped Browserstack local connection.')
