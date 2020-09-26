34567e018e93a9fd4e846dd77799cbb3cec4a20f
Bas Maas <bas.maas@sogeti.com>
2020-09-26 11:55:36 +0200

    update pipeline by tagging images with commit msg

--- .semaphore/scripts/export-git-env-vars.sh
+++ .semaphore/scripts/export-git-env-vars.sh
@@ -2,3 +2,4 @@ export GIT_COMMIT_SHA=$(git rev-parse --short HEAD)
 export GIT_COMMIT_SHA_LONG=$(git rev-parse HEAD)
 export GIT_COMMIT_MSG=$(git show -s --format=%B ${GIT_COMMIT_SHA})
 export GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+export IMAGE_TAG=`echo "${GIT_COMMIT_MSG}" | sed -e "s/ /-/g" | cut -c1-71 | sed "s/$/.${GIT_BRANCH}/" | sed "s/$/-${GIT_COMMIT_SHA}/"` 
