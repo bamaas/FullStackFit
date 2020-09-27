075290d6ee7a24f3e5e2cb9a2f4833f537b3615a
Bas Maas <bas.maas@sogeti.com>
2020-09-27 21:15:45 +0200

    updated image tag script

--- .semaphore/scripts/export-git-env-vars.sh
+++ .semaphore/scripts/export-git-env-vars.sh
@@ -2,4 +2,4 @@ export GIT_COMMIT_SHA=$(git rev-parse --short HEAD)
 export GIT_COMMIT_SHA_LONG=$(git rev-parse HEAD)
 export GIT_COMMIT_MSG=$(git show -s --format=%B ${GIT_COMMIT_SHA} | head -n 1)
 export GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-export IMAGE_TAG=`echo "${GIT_COMMIT_MSG}" | sed -e "s/ /-/g" | cut -c1-71 | sed "s/$/-${GIT_COMMIT_SHA}/" | sed "s/$/-${GIT_BRANCH}/" | tr -d "'" | tr -d "#" | tr -d "(" | tr -d ")" | sed "s/https:\/\/github.com\/bamaas\/FullStackFit/remote/g"`
+export IMAGE_TAG=`echo "${GIT_COMMIT_MSG}" | sed -e "s/ /-/g" | cut -c1-71 | sed "s/$/-${GIT_COMMIT_SHA}/" | sed "s/$/-${GIT_BRANCH}/" | tr -d "'" | tr -d "#" | tr -d "(" | tr -d ")" | sed "s/\//-/g" | sed "s/https:\/\/github.com\/bamaas\/FullStackFit/remote/g`
