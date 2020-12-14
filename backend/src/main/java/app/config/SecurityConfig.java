38ded55a298878dbbde6a4b319f101e261209208
Bas Maas <bas.maas@sogeti.com>
2020-12-14 08:20:16 +0100

    backend requires role for all requests

--- backend/src/main/java/app/config/SecurityConfig.java
+++ backend/src/main/java/app/config/SecurityConfig.java
@@ -54,7 +54,7 @@ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter
         super.configure(http);
         http
                 .authorizeRequests()
-                .antMatchers("/entry*").hasRole("user")
+                .antMatchers("*").hasRole("user")
                 .anyRequest().permitAll();
     }
 }
