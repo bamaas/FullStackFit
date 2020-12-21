9b2f4830a200fcb908103ae488c54671754c6b7b
Bas Maas <bas.maas@sogeti.com>
2020-12-21 23:46:24 +0100

    added liquibase and init sql script now adds user table

--- database/init.sql
+++ database/init.sql
@@ -1,14 +1,20 @@
 CREATE SCHEMA iam;
 
+CREATE TABLE public.user (
+    id VARCHAR NOT NULL UNIQUE PRIMARY KEY
+);
+
+CREATE UNIQUE INDEX user_id ON public.user(id);
+
 CREATE TABLE public.entry (
-    id SERIAL PRIMARY KEY, 
-    date TIMESTAMP, 
-    weight REAL, 
+    id SERIAL NOT NULL UNIQUE PRIMARY KEY, 
+    date TIMESTAMP,
+    weight REAL,
     note TEXT, 
     year SMALLINT, 
     week SMALLINT, 
-    user_id VARCHAR, 
-    FOREIGN KEY (user_id) REFERENCES iam.user_entity(id)
+    user_id VARCHAR,
+    FOREIGN KEY (user_id) REFERENCES public.user(id)
 );
 
 CREATE TABLE public.weekly_average (
@@ -16,9 +22,9 @@ CREATE TABLE public.weekly_average (
     week SMALLINT, 
     weight_average REAL, 
     weight_measurement_count SMALLINT, 
-    user_id VARCHAR, 
+    user_id VARCHAR,
     PRIMARY KEY (year, week, user_id), 
-    FOREIGN KEY (user_id) REFERENCES iam.user_entity(id)
+    FOREIGN KEY (user_id) REFERENCES public.user(id)
 );
 
 CREATE FUNCTION update_weekly_average() RETURNS trigger AS $BODY$
@@ -37,7 +43,7 @@ DECLARE avg_weight REAL;
         SELECT ROUND(AVG(weight)::numeric,1) FROM public.entry WHERE year = NEW.year AND week = NEW.week AND user_id = NEW.user_id INTO avg_weight;
         UPDATE public.weekly_average SET weight_average = avg_weight WHERE week = NEW.week AND year = NEW.year AND user_id = NEW.user_id; 
         IF NOT FOUND THEN 
-            INSERT INTO weekly_average (year, week, weight_average, user_id) VALUES (NEW.year, NEW.week, avg_weight, NEW.user_id); 
+            INSERT INTO public.weekly_average (year, week, weight_average, user_id) VALUES (NEW.year, NEW.week, avg_weight, NEW.user_id); 
         END IF;
         -- set weight_measurement_count
         UPDATE public.weekly_average SET weight_measurement_count = (SELECT COUNT(weight) FROM public.entry WHERE year = NEW.year AND week = NEW.week AND user_id = NEW.user_id) WHERE year = NEW.year AND week = NEW.week AND user_id = NEW.user_id;
@@ -87,6 +93,7 @@ CREATE FUNCTION update_entry_year_and_week() RETURNS trigger AS $BODY$
         IF NEW.date IS NULL THEN
             RAISE EXCEPTION 'date cannot be null';
         END IF;
+        -- extract year and week from the date and update the value
         NEW.year := (SELECT EXTRACT(YEAR FROM NEW.date));
 		NEW.week := (SELECT FLOOR((DATE_PART('doy', NEW.date)) / 7));
         RETURN NEW;
@@ -95,3 +102,19 @@ $BODY$ LANGUAGE plpgsql;
 
 CREATE TRIGGER update_entry_year_and_week BEFORE INSERT OR UPDATE ON public.entry
     FOR EACH ROW EXECUTE PROCEDURE update_entry_year_and_week();
+
+CREATE FUNCTION set_user_id() RETURNS trigger AS $BODY$
+    BEGIN
+        IF NEW.user_id IS NULL THEN
+            RAISE EXCEPTION 'user_id cannot be null';
+        END IF;
+        -- insert user_id in user table if not exists
+        IF NOT EXISTS (SELECT FROM public.user WHERE id = NEW.user_id) THEN
+            INSERT INTO public.user (id) VALUES (NEW.user_id);
+        END IF;
+        RETURN NEW;
+    END;
+$BODY$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_user_id BEFORE INSERT on public.entry
+    FOR EACH ROW EXECUTE PROCEDURE set_user_id();
