10fc8253dad55d2541751d9d5a4098faf08fdafd
Bas Maas <bas.maas@sogeti.com>
2020-12-22 20:42:07 +0100

    new semaphore ci file two

--- .semaphore/scripts/rollout.sh
+++ .semaphore/scripts/rollout.sh
@@ -2,4 +2,5 @@
 # $1 = namespace
 # $2 = workload
 # $3 = name
-kubectl -n $1 rollout restart $2 $3 --insecure-skip-tls-verify && kubectl -n $1 rollout status $2/$3 --insecure-skip-tls-verify
+# kubectl -n $1 rollout restart $2 $3 --insecure-skip-tls-verify && kubectl -n $1 rollout status $2/$3 --insecure-skip-tls-verify
+kubectl -n $1 rollout status $2/$3 --insecure-skip-tls-verify
