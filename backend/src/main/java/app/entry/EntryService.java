7e51480dfa88316252cc71ef57963735d69d4876
Bas Maas <bas.maas@sogeti.com>
2020-12-23 20:09:44 +0100

    readme change and add default schema to backend

--- backend/src/main/java/app/entry/EntryService.java
+++ backend/src/main/java/app/entry/EntryService.java
@@ -7,10 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
-
-import java.security.Principal;
 import java.util.*;
 
 @Service
