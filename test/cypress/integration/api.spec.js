1c8a79e76a0d6a84cdb7941714c3b40f8703bdf9
Bas Maas <bas.maas@sogeti.com>
2020-12-25 10:40:51 +0100

    fix api test

--- test/cypress/integration/api.spec.js
+++ test/cypress/integration/api.spec.js
@@ -51,7 +51,8 @@ context('FitTrack', () => {
     it(['regression'], 'post an entry', () => {
       const weight = (Math.floor(Math.random() * 99) + 1)
       const note  = Math.random().toString().substr(2, 8);
-      const year = Math.floor(Math.random() * 9000) + 1;
+      let year = (Math.floor(Math.random() * 9000) + 1).toString();
+      year = year.replace(/^0+/, "1");
       const date = `${year}-01-01T15:12:30`;
       cy.get('@userId').then(userId => {
           cy.get('@accessToken').then(token => {
@@ -149,7 +150,8 @@ context('FitTrack', () => {
 
       const weight = (Math.floor(Math.random() * 99) + 1)
       const note  = Math.random().toString().substr(2, 8);
-      const year = Math.floor(Math.random() * 9000) + 1;
+      let year = (Math.floor(Math.random() * 9000) + 1).toString();
+      year = year.replace(/^0+/, "1");
       const date = `${year}-01-01T15:12:30`
 
       cy.get('@entryId').then(entryId => {
